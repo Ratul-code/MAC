e7277fe78b4210579582909ef37909fa0bc8b453
Ratul <rafiratul01@gmail.com>
2021-03-13 06:41:42 +0600

    made it better4

--- My_Awesome_project/settings.py
+++ My_Awesome_project/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = ['mac-deployed.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'shop.apps.ShopConfig',
-    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop.apps.ShopConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
